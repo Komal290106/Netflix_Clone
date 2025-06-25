1e3650e16c343b533f22b8a892533578f0883577
Komal <komal1133.becse24@chitkara.edu.in>
2025-06-25 13:29:59 +0530

    moved apt keys to .env

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,15 @@ import { addDoc, collection, getFirestore } from "firebase/firestore/lite";
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoF_vMd0Y7haRe-ErgZRS8ogJT5tjReNQ",
-  authDomain: "netflix-clone-e331c.firebaseapp.com",
-  projectId: "netflix-clone-e331c",
-  storageBucket: "netflix-clone-e331c.firebasestorage.app",
-  messagingSenderId: "958090119070",
-  appId: "1:958090119070:web:79c4effc8f880275a9388f",
-  measurementId: "G-NJ27QJEQJL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 
 const app = initializeApp(firebaseConfig);
