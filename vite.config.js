bd5deb6bbc4a36f560312cfcc0f5ce4921c3de83
Komal Kaur <dhillonkomal59@gamil.com>
2025-09-15 15:19:12 +0530

    made some changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <- this line helps avoid the black screen issue on deploy
+  base: './',  // 👈 fix for assets & CSS on Vercel
 })
